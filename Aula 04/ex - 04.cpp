6c6ed8f52f0235ef281c157e1afc4596d10f48f2
leonardo-programa <leo2008santos@hotmail.com>
2025-08-13 14:17:12 -0300

    Add files via upload

---
+++ Aula 04/ex - 04.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int contador = 1, apr = 0;
+    cout << "--- CONCURSO IBGE ---" << endl;
+    
+    while(1){
+        int port, mat; float red;
+        
+        cout << "Candidato " << contador << endl;
+        cout << "Portugues: ";
+        cin >> port;
+        if (port < 0){
+            break;
+        }
+        cout << "Matematica: ";
+        cin >> mat;
+        cout << "Redacao: ";
+        cin >> red;
+        
+        if (mat >= 0.6 * 35 && port >= 0.8 * 50 && red >= 7){
+            apr++;
+        }
+        contador++;
+    }
+    cout << "\nQuantidade de aprovados: " << apr << endl;
+    
+    
+    return 0;
+}
