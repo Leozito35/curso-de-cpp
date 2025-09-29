7299a0c9b06142bac28a4550bba1f077fa50909b
Leozito35 <leo2008santos@hotmail.com>
2025-09-29 18:23:54 -0300

    Update Projeto.cpp

--- Projeto Final/Projeto.cpp
+++ Projeto Final/Projeto.cpp
@@ -7,29 +7,23 @@ using namespace std;
 
 const int MAX = 100; 
 
-class Produto{
+class Item{
 protected:
     string nome;
     float valor;
-    int quantidade;
 public:
-    //Construtores
-    Produto(){
+    Item(){
         nome = "Indefinido";
-        valor = 0.0;
-        quantidade = 0;
+        valor = 0;
     }
-    Produto(string nome, float valor, int quantidade){
+    Item(string nome, float valor){
         this->nome = nome;
         this->valor = valor;
-        this->quantidade = quantidade;
     }
 
-    //Encapsulamento
     string getNome() const { return nome;}
     float getValor() const { return valor;}
-    int getQtd() const { return quantidade;}
-
+    
     void setNome(string n){ nome = n; }
     void setValor(float valor){
         if (valor > 0 && valor < 100000){
@@ -39,6 +33,23 @@ public:
             this->valor = 0.0;
         }
     }
+};
+class Produto : public Item{
+private:
+    int quantidade;
+public:
+    //Construtores
+    Produto() : Item(){
+        quantidade = 0;
+    }
+    Produto(string nome, float valor, int quantidade) : Item(nome, valor){
+        this->quantidade = quantidade;
+    }
+
+    //Encapsulamento
+    int getQtd() const { return quantidade;}
+
+    
     void setQtd(int quantidade){
         if (quantidade > 0 && quantidade < 1000){
             this->quantidade = quantidade;
@@ -56,19 +67,18 @@ public:
         cout << "Quantidade: " << quantidade << endl << endl;
     }
 };
-
-class Venda : public Produto{
+class Venda : public Item{
 private:
     int qtdVendida;
 public:
-    Venda(string nome, float valor, int quantidade, int qtdVendida) : Produto(nome, valor, quantidade){
+    Venda(string nome, float valor, int qtdVendida) : Item(nome, valor){
         this->qtdVendida = qtdVendida;
     }
 
     int getQtdVendida() const { return qtdVendida;}
     void setQtdVendida(int qtdVendida) { this->qtdVendida = qtdVendida;}
-
-    float calcularVenda(int qtdVendida, float valor) { return valor * qtdVendida;}
+    
+    float calcularVenda() { return valor * qtdVendida;}
 };
 
 int menu();
@@ -80,14 +90,13 @@ void excluirProduto(vector<Produto> &estoque);
 void buscarProduto(const vector<Produto> &estoque);
 void registrarVenda(vector<Produto> &estoque, vector<Venda> &vendas);
 void relatorio(vector<Produto> &estoque, vector<Venda> &vendas);
-
-void salvarDados();
-void carregarDados();
+void salvarDados(vector<Produto> &estoque, vector<Venda> &vendas);
+void carregarDados(vector<Produto> &estoque, vector<Venda> &vendas);
 
 
 int main(){
     vector <Produto> estoque; vector <Venda> vendas;
-    carregarDados();
+    carregarDados(estoque, vendas);
     int opcao;
     do{
         opcao = menu();
@@ -118,15 +127,17 @@ int main(){
                 break;
             case 9:
                 cout << "Saindo..." << endl;
-                salvarDados();
+                salvarDados(estoque, vendas);
                 break;
+            default:
+                cout << "Opção inválida." << endl;
         }
     } while(opcao != 9);
 
     return 0;
 }
 int menu(){
-    cout << "=============== Gerenciamento de loja ===============" << endl;
+    cout << "\n=============== Gerenciamento de loja ===============" << endl;
     cout << "1 - Cadastrar produto        6 - Busca de produto" << endl;
     cout << "2 - Atualizar produto        7 - Registrar venda" << endl;
     cout << "3 - Adicionar no estoque     8 - Relatório de vendas" << endl;
@@ -137,11 +148,12 @@ int menu(){
     int opcao;
     cout << "Escolha a opcao: ";
     cin >> opcao;
+    cout << endl;
     return opcao;
 }
 void criarProduto(vector<Produto> &estoque) {
-    if (estoque.size() > 100){
-        cout << "Estoque cheio!";
+    if (estoque.size() >= 100){
+        cout << "Estoque cheio!" << endl << endl;
         return;
     }
     
@@ -185,6 +197,7 @@ void atualizarProduto(vector<Produto> &estoque) {
             a.setNome(novoNome);
             a.setQtd(novaQtd);
             a.setValor(novoValor);
+            return;
         }
     }
     cout << "Produto nao encontrado!";
@@ -228,6 +241,20 @@ void excluirProduto(vector<Produto> &estoque){
         cout << "Nenhum produto cadastrado!" << endl;
         return;
     }
+
+    cin.ignore();
+    string nomeBusca;
+    cout << "\nDigite o nome do produto que deseja remover: ";
+    getline(cin, nomeBusca);
+    
+    for (auto it = estoque.begin(); it != estoque.end(); ++it){
+        if (it->getNome() == nomeBusca){
+          estoque.erase(it);
+          cout << "Produto removido com sucesso!\n";
+          return;
+        }
+    }
+    cout << "Produto não encontrado.\n";
 }
 void buscarProduto(const vector<Produto> &estoque){
     if (estoque.empty()){
@@ -242,8 +269,10 @@ void buscarProduto(const vector<Produto> &estoque){
     for (const auto &a : estoque){
         if (nomeBusca == a.getNome()){
             a.mostrarProduto();
+            return;
         }
     }
+    cout << "Produto não encontrado!" << endl;
 }
 void registrarVenda(vector<Produto> &estoque, vector<Venda> &vendas){
     if(estoque.empty()){
@@ -253,25 +282,123 @@ void registrarVenda(vector<Produto> &estoque, vector<Venda> &vendas){
     string nomeVenda;
     cout << "Digite o produto vendido: ";
     getline(cin, nomeVenda);
+    
+    for (auto &produto : estoque){
+        if (produto.getNome() == nomeVenda){
+            int qtd;
+            cout << "Digite a quantidade desejada: ";
+            cin >> qtd;
+            if (produto.getQtd() < qtd){
+                cout << "Quantidade acima do estoque! Venda não realizada!" << endl;
+                cout << "Quantidade disponivel: " << produto.getQtd() << endl;
+                return;
+            }
+            //atualiza estoque
+            produto.setQtd(produto.getQtd() - qtd);
+            
+            //registra venda
+            vendas.push_back(Venda(produto.getNome(), produto.getValor(), qtd));
+            cout << "Venda registrada com sucesso! Total de " << qtd * produto.getValor() << "$" << endl;
+        }
+    }
+    
 }
 void relatorio(vector<Produto> &estoque, vector<Venda> &vendas){
-
+    cout << "=== Relatorio Geral ===" << endl;
+    if (estoque.empty()){
+        cout << "Sem produtos cadastrados." << endl;
+    } else {
+        cout << "Produtos cadastrados: " << estoque.size() << endl;
+        for(auto &produto : estoque){
+            produto.mostrarProduto();
+        }
+    } 
+    
+    if (vendas.empty()){
+        cout << "Não foi realizado nenhuma venda." << endl;
+    } else {
+        cout << "Vendas realizadas: " << vendas.size() << endl;
+        float soma = 0;
+    for (auto &venda : vendas){
+        soma += venda.calcularVenda();
+    }
+    cout << "Total ganho: " << soma << "$" << endl;
+    }
+    
 }
 void salvarDados(vector<Produto> &estoque, vector<Venda> &vendas){
-    ofstream arquivo("gerenciamento.txt");
+    //salvar estoque
+    ofstream arquivoEstoque("estoque.txt");
 
-    if (!arquivo.is_open()){
-    cout << "Erro ao abrir o arquivo para salvar!\n";
-    return;
+    if (!arquivoEstoque.is_open()){
+        cout << "Erro ao abrir o arquivo para salvar!\n";
+        return;
     }
 
-    for (const auto &a : estoque){
-    arquivo << a.getNome() << "," << a.getQtd() << "," << a.getValor() << "\n";
+    for (const auto &produto : estoque){
+        arquivoEstoque << produto.getNome() << "," << produto.getQtd() << "," << produto.getValor() << "\n";
     }
 
-    arquivo.close();
-    cout << "Dados salvos com sucesso!\n";
+    arquivoEstoque.close();
+    cout << "Dados do estoque salvos com sucesso!" << endl;
+    
+    //salvar vendas
+    ofstream arquivoVendas("vendas.txt"); // CORREÇÃO: ; adicionado
+    if (!arquivoVendas.is_open()){
+        cout << "Erro ao abrir o arquivo para salvar!\n";
+        return;
+    }
+    
+    for (const auto &venda : vendas){
+        arquivoVendas << venda.getNome() << "," << venda.getQtdVendida() << "," << venda.getValor() << "\n"; // CORREÇÃO: linha completa
+    }
+    arquivoVendas.close(); // CORREÇÃO: arquivoVendas
+    cout << "Dados de vendas salvos com sucesso!" << endl;
 }
-void carregarDados(){
+void carregarDados(vector<Produto> &estoque, vector<Venda> &vendas){
+    // Carregar estoque
+    ifstream arquivoEstoque("estoque.txt");
 
+    if (!arquivoEstoque.is_open()){
+        cout << "Nenhum dado de estoque encontrado. Iniciando lista vazia.\n";
+    } else {
+        string linha;
+        while (getline(arquivoEstoque, linha)) {
+            size_t pos1 = linha.find(",");
+            size_t pos2 = linha.find(",", pos1 + 1);
+
+            if (pos1 != string::npos && pos2 != string::npos) {
+                string nome = linha.substr(0, pos1);
+                int quantidade = stoi(linha.substr(pos1 + 1, pos2 - pos1 - 1));
+                float valor = stof(linha.substr(pos2 + 1));
+
+                estoque.push_back(Produto(nome, valor, quantidade));
+            }
+        }
+        arquivoEstoque.close();
+        cout << "Dados do estoque carregados com sucesso!\n";
+    }
+    
+    // Carregar vendas
+    ifstream arquivoVendas("vendas.txt");
+    
+    if (!arquivoVendas.is_open()){
+        cout << "Nenhum dado de vendas encontrado. Iniciando lista vazia.\n";
+    } else {
+        string linha;
+        while (getline(arquivoVendas, linha)) {
+            size_t pos1 = linha.find(",");
+            size_t pos2 = linha.find(",", pos1 + 1);
+
+            if (pos1 != string::npos && pos2 != string::npos) {
+                string nome = linha.substr(0, pos1);
+                int qtdVendida = stoi(linha.substr(pos1 + 1, pos2 - pos1 - 1));
+                float valor = stof(linha.substr(pos2 + 1));
+
+                vendas.push_back(Venda(nome, valor, qtdVendida));
+            }
+        }
+        arquivoVendas.close();
+        cout << "Dados de vendas carregados com sucesso!\n";
+    }
 }
